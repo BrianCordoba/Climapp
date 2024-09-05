ccfb19588ce396d9f0cf9e892905ace98d5a824d
BrianCordoba <brianlyo77@gmail.com>
2024-09-05 20:41:53 -0300

    pal cel

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,32 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    VitePWA({
+      manifest: {
+          name: 'ClimApp',
+          short_name: 'Administración',
+          description: 'Sistema de administración de ClimApp',
+          theme_color: '#0f172a',
+          start_url: '/',
+          icons: [
+              {
+                  src: "/android-chrome-512x512.png",
+                  sizes: "512x512",
+                  type: "image/png",
+                  purpose: "any maskable"
+              },
+              {
+                  src: "/android-chrome-192x192.png",
+                  sizes: "192x192",
+                  type: "image/png",
+                  purpose: "any maskable"
+              }
+          ]
+      }
+  })
+  ],
 })
